b5c96fc63e42b06879b7b07e71b47da2d389ea42
PhiHo-eng <74133157+PhiHo-eng@users.noreply.github.com>
2021-06-10 14:12:32 +0200

    Add files via upload

---
+++ Auswertung_Modelle_Hofer/Cantilever_OC_Youngs_modulus_Abhaengigkeit/TopOptPostProc4Hofer.py
@@ -0,0 +1,83 @@
+
+from KratosTools.KratosVisualization \
+    import VisualizeMesh, VisualizeModalResults, VisualizeNodalResults, \
+        VisualizeElementalResults
+import pyvista as pv
+
+# "Gobal" settings
+color = "#1F77B4"   # I chose my standard blue, feel free to change
+ShowPlot = False    # Plot figures to screen. If false, figures will only be saved. 
+WindowSize = [6000, 9000]  # Vertically oriented, for horizontally oriented models [10000, 5000]
+                            # These are the maxima on my computer. 
+                            # Follow here to see if solution available:
+                            # https://github.com/pyvista/pyvista-support/issues/423
+
+
+
+meshFig = VisualizeMesh()
+meshFig.name = "TopOpt_ViertelModel_iso"
+meshFig.vtkFolder = "."    # in this folder
+meshFig.vtkFile = "Canitlver_alter_filter.vtu"
+meshFig.showPlot = ShowPlot
+meshFig.showNumber = False
+meshFig.showNodes = False
+meshFig.showMesh = True
+meshFig.showVolume = True
+meshFig.VolumeColor = color
+meshFig.WindowSize = WindowSize
+meshFig.view =  (1, 1, 1)
+meshFig.make()
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_iso2"
+meshFig.view =  (1, -1, 1)
+meshFig.make()
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_iso3"
+meshFig.view =  (-1, -1, 1)
+meshFig.make()
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_iso4"
+meshFig.view =  (-1, 1, 1)
+meshFig.make()
+
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_side1"
+meshFig.view =  (1, 0, 0)
+meshFig.make()
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_side2"
+meshFig.view =  (0, 1, 0)
+meshFig.make()
+
+meshFig.name = "TopOpt_Canitlver_alter_filter_top"
+meshFig.view =  (0, 0, 1)
+meshFig.make()
+
+
+# Wireframe plot of mesh
+meshFig = VisualizeMesh()
+meshFig.name = "TopOpt_Canitlver_alter_filter_WireFrame_iso"
+meshFig.vtkFolder = "."    # in this folder
+meshFig.vtkFile = "Canitlver_alter_filter.vtu"
+meshFig.showPlot = ShowPlot
+meshFig.showNumber = True
+meshFig.showNodes = False
+meshFig.WindowSize = WindowSize
+meshFig.make()
+       
+
+# Density plots
+densFig = VisualizeElementalResults()
+densFig.name = "TopOpt_Canitlver_alter_filter_Dens_iso"
+densFig.vtkFolder = "."    # in this folder
+densFig.vtkFile = "alter_filter0024.vtu"
+densFig.showPlot = ShowPlot
+densFig.showNumber = False
+densFig.showNodes = False
+densFig.warpResponse = False
+densFig.TransparentVolume = True
+densFig.WindowSize = WindowSize
+densFig.Responses = ["X_PHYS"]
+densFig.BarTitles = ["density\n(normalized)"]
+densFig.FileNameAdd = ["density"]
+densFig.make()
